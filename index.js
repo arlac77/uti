e2444edd99873b9707bc19d37bbf4eab44f0cba7
Markus Felten <Markus.Felten@gmx.de>
2015-06-08 14:16:40 +0200

    1st. running test

--- index.js
+++ index.js
@@ -5,39 +5,72 @@
 const fs = require("fs");
 const path = require("path");
 
-exports.initialize = function () {
+const RootUTI = {
+  toString() {
+      return this.name
+    },
+    toJSON() {
+      return {
+        name: this.name,
+        conformsTo: this.conformsTo
+      };
+    }
+};
+
+exports.initialize = function (options) {
+  if (!options) { 
+    options = {};
+  }
 
   const registry = {};
 
   const uti = {
     getUTI(name) {
-      return registry[name];
-    },
-    conformesTo(a, b) {
-      const utiA = registry[a];
-      if(!utiA) { return false; }
-      return utiA.conformsTo[b];
-    }, loadDefinitions(fileName) {
-      return new Promise(function (resolve, reject) {
-        console.log(`load: ${fileName}`);
-        fs.createReadStream(fileName, {
-          encoding: "utf8"
-        }, function (error, data) {
-          console.log(`${error} ${data}`);
-          if(error) { reject(error); return; }
+        return registry[name];
+      },
+      conformesTo(a, b) {
+        const utiA = registry[a];
+        if (!utiA) {
+          return false;
+        }
+        return utiA.conformsTo[b];
+      }, loadDefinitions(fileName) {
+        return new Promise(function (resolve, reject) {
+          fs.readFile(fileName, {
+            encoding: "utf8"
+          }, function (error, data) {
+            if (error) {
+              reject(error);
+              return;
+            }
 
-          const u = JSON.parse(data);
+            const u = JSON.parse(data);
 
-          for (let i in u) {
-            const nu = { conformsTo: { }};
-            registry[i] = nu;
-          }
+            for (let i in u) {
+              const nu = Object.create(RootUTI, {
+                name: {
+                  value: i
+                },
+                conformsTo: {
+                  value: u.conformsTo
+                }
+              });
 
-          resolve(uti);
+              //console.log(i);
+              registry[i] = nu;
+            }
+
+            resolve(uti);
+          });
         });
-      });
-    }
+      }
   };
 
-  return uti.loadDefinitions(path.join(__dirname, 'publicUTI.json'));
+  const p = uti.loadDefinitions(path.join(__dirname, 'publicUTI.json'));
+
+  if (options.definitionFile) {
+    return p.then(uti.loadDefinitions(options.definitionFile));
+  }
+
+  return p;
 };
--- test/uti.js
+++ test/uti.js
@@ -9,21 +9,38 @@ const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
 
+let path = require('path');
 let uti = require('../index.js');
 
 describe('uti', function () {
 
-  describe('buildin utis', function () {
+  describe('buildin UTIs', function () {
     it('should be present', function (done) {
       uti.initialize().then(function (uti) {
         const json = uti.getUTI('public.json');
         should.exist(json);
         done();
-      },function(error) {
+      }, function (error) {
         console.log(`${error}`);
         assert(false);
         done();
-        });
+      });
+    });
+  });
+
+  describe('addtitional UTIs', function () {
+    it('should be present', function (done) {
+      uti.initialize({
+        definitionFile: path.join(__dirname, 'fixtures', 'uti.json')
+      }).then(function (uti) {
+        const myUTI = uti.getUTI('com.mydomain.sample');
+        should.exist(myUTI);
+        done();
+      }, function (error) {
+        console.log(`${error}`);
+        assert(false);
+        done();
+      });
     });
   });
 
