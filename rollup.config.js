86841a4699c9c44697e15fe82f672d7a36d94ad7
Markus Felten <Markus.Felten@gmx.de>
2018-07-17 00:11:31 +0200

    chore(package): add rollup-plugin-executable@^1.2.0 chore(rollup): import executable

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import executable from 'rollup-plugin-executable';
 import pkg from './package.json';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
