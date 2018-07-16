934893061ddc2c2ba133ac4fee6f278287909f31
Markus Felten <Markus.Felten@gmx.de>
2018-07-16 22:45:13 +0200

    chore(package): add rollup-plugin-multi-entry@^2.0.2 chore(package): add rollup-plugin-istanbul@^2.0.1 chore(rollup): import multiEntry chore(rollup): import istanbul chore(rollup): add multiEntry to plugins chore(rollup): add istanbul to plugins

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,10 +1,15 @@
+import istanbul from 'rollup-plugin-istanbul';
+
+import multiEntry from 'rollup-plugin-multi-entry';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: 'tests/uti-test.js',
   external: ['ava', 'fs', 'path', 'util'],
-  plugins: [resolve(), commonjs()],
+  plugins: [resolve(), commonjs(), multiEntry(), istanbul({
+    exclude: ['tests/**/*-test.js']
+  })],
 
   output: {
     file: 'build/uti-test.js',
