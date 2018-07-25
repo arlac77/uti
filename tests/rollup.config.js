3eab9338825d1917fa2a4ee076805772ad9c1716
Markus Felten <Markus.Felten@gmx.de>
2018-07-25 18:07:42 +0200

    chore(rollup): reformat only

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,5 +1,4 @@
 import istanbul from 'rollup-plugin-istanbul';
-
 import multiEntry from 'rollup-plugin-multi-entry';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
