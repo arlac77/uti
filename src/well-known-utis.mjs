024c3152fc1b4d88a723100a316bfb6b5b775bc5
Markus Felten <Markus.Felten@gmx.de>
2023-01-24 18:11:42 +0100

    feat: oci images

--- src/well-known-utis.mjs
+++ src/well-known-utis.mjs
@@ -439,6 +439,24 @@ const types = [
     fileNameExtension: [".dmg", ".smi", ".img"],
     mimeType: "application/x-apple-diskimage"
   },
+  { name: "public.oci.image.layer.v1",
+    conformsTo: ["public.data"],
+  },  
+  { name: "public.oci.image.layer.v1.tar",
+    conformsTo: ["public.oci.image.layer.v1"],
+    filNameExtension: [".tar"],
+    mimeType: "application/vnd.oci.image.layer.v1.tar"
+  },
+  { name: "public.oci.image.layer.v1.tar.gzip",
+    conformsTo: ["public.oci.image.layer.v1"],
+    filNameExtension: [".tar.gz"],
+    mimeType: "application/vnd.oci.image.layer.v1.tar+gzip"
+  },
+  { name: "public.oci.image.layer.v1.tar.zstd",
+    conformsTo: ["public.oci.image.layer.v1"],
+    filNameExtension: [".tar.zstd"],
+    mimeType: "application/vnd.oci.image.layer.v1.tar+zstd"
+  },
   {
     name: "public.security.private-key",
     conformsTo: "public.data"
